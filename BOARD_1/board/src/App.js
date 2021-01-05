a4cb8132b9348de1e08f177af927a93f0a64a0a6
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-01-06 05:43:15 +0900

    board version 1.1

--- BOARD_1/board/src/App.js
+++ BOARD_1/board/src/App.js
@@ -1,18 +1,56 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [Content, setContent] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [viewContent, setViewContent] = useState([]);
+
+  const getValue = (e) => {
+    const { name, value } = e.target;
+    setContent({
+      ...Content,
+      [name]: value,
+    });
+    console.log(Content);
+  };
   return (
     <div className="App">
       <h1>게시판</h1>
       <div className="movie-container">
-        <h2>제목</h2>
-        <div>내용</div>
+        {viewContent.map((Element) => (
+          <div>
+            <h2>{Element.title}</h2>
+            <div>{Element.content}</div>
+          </div>
+        ))}
       </div>
       <div className="form-wrapper">
-        <input className="title-input" type="text" placeholder="제목" />
-        <textarea className="text-area" placeholder="내용"></textarea>
+        <input
+          className="title-input"
+          type="text"
+          placeholder="제목"
+          onChange={getValue}
+          name="title"
+        />
+        <textarea
+          className="text-area"
+          placeholder="내용"
+          onChange={getValue}
+          name="content"
+        ></textarea>
       </div>
-      <button className="submit-button">입력</button>
+      <button
+        className="submit-button"
+        onClick={() => {
+          setViewContent(viewContent.concat({ ...Content }));
+        }}
+      >
+        입력
+      </button>
     </div>
   );
 }
