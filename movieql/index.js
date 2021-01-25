6ce0f0eaa0e94879f9b8cdc4f0c32cb47a25cd64
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-01-25 12:33:57 +0900

    0.4Chap

--- movieql/index.js
+++ movieql/index.js
@@ -1,1 +1,5 @@
-const graphqlYoga = require("graphql-yoga");
+import { GraphQLServer } from "graphql-yoga";
+
+const server = new GraphQLServer({});
+
+server.start(() => console.log("Graphql Server Running"));
